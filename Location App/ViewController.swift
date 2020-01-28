16353b02217a4a300055c9f96906b5e4031b2c5a
An Nguyen <an.nguyen6@mavs.uta.edu>
2020-01-27 19:38:06 -0600

    Initial Commit

---
+++ Location App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location App
+//
+//  Created by An Nguyen on 1/27/20.
+//  Copyright © 2020 An Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
