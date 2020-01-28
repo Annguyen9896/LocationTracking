534e8fff0a5cb2e1ca86a047eaa5d6a4071cdb48
An Nguyen <an.nguyen6@mavs.uta.edu>
2020-01-27 20:51:48 -0600

    first commit

--- Location App/ViewController.swift
+++ Location App/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    
+    let locationManager = CLLocationManager()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.requestAlwaysAuthorization()
+
+//        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+}
+    
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            print(location.coordinate)
+        }
+}
         // Do any additional setup after loading the view.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied) {
+            showLocationDisablePopUp()
+        }
+}
+    func showLocationDisablePopUp(){
+        let alertController = UIAlertController(title: "Backgroud location access denied", message: "Inorder to send notification, we need your location", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil )
+        
+        let openAction = UIAlertAction(title: "Open Setting", style: .default) {(action) in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertController.addAction(openAction)
+        self.present(alertController, animated: true, completion: nil)
     }
-
-
 }
 
